552e214b568e5f97e82dd6ae899c0c278e61e98f
Tanwinder Sidhu <sidhu@Gurpreets-MacBook-Air.local>
2018-05-22 10:10:36 -0500

    add create component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import { BrowserRouter as Router, Switch, NavLink, Route, Redirect } from 'react
 import CreateComponent from './component/CreateComponent';
 import EditComponent from './component/EditComponent';
 import IndexComponent from './component/IndexComponent';
+import Home from './component/home';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
+        <div className="container">
           <h2>Welcome to my App</h2>
-          <ul>
-          <li><NavLink to={'/'}>Home</NavLink></li>
-            <li><NavLink to={'/create'}>Create</NavLink></li>
-            <li><NavLink to={'/index'}>List</NavLink></li>
-            <li><NavLink to={'/edit/:id'}>Edit</NavLink></li>
-          </ul>
-          <hr />
+          <nav className="navbar navbar-expand-xl navbar-dark bg-info" >
+          <NavLink className="navbar-brand" to={'/'}>ReactApp</NavLink>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to={'/'}>Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={'/create'}>Create</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={'/index'}>List</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to={'/edit/:id'}>Edit</NavLink>
+              </li>
+            </ul>
+          </nav>
+          {/* <hr /> */}
           <main>
             <Switch>
-              <Route path='/create' exact component={CreateComponent} />
-              <Route path='/edit/:id' exact component={EditComponent} />
-              <Route path='/index' exact component={IndexComponent} />
-              <Redirect to='/create' />
+              <Route path='/' exact component={Home} /> 
+              <Route path='/create' component={CreateComponent} />
+              <Route path='/edit/:id' component={EditComponent} />
+              <Route path='/index' component={IndexComponent} />
+              <Redirect to='/' />
             </Switch>
           </main>
         </div>
